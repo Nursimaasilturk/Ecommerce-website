4e1830421107affe6e72da8a18741cb620f9e0dd
Nursima Asilturk <nursima.asilturk99@gmail.com>
2022-08-03 01:27:51 +0300

    shop page and product details page was done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const menu = document.querySelector(".categories ul");
+const mainImg = document.getElementById("MainImg");
+const smallImg = document.getElementsByClassName("small-img");
 if (bar) {
   bar.addEventListener("click", () => {
     menu.classList.add("active");
@@ -11,3 +13,15 @@ if (close) {
     menu.classList.remove("active");
   });
 }
+smallImg[0].onclick = function () {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  mainImg.src = smallImg[3].src;
+};
